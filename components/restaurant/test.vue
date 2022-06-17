de85288ad33048cf2e564a973fa08f2f09980aa9
Kevin <tgodkev@gmail.com>
2022-06-17 16:42:59 -0400

    test

--- components/restaurant/test.vue
+++ components/restaurant/test.vue
@@ -3,6 +3,26 @@
     this is a huge test
 
   </div>
+  <div>
+    this is a huge test
+
+  </div>
+  <div>
+    this is a huge test
+
+  </div>
+  <div>
+    this is a huge test
+
+  </div>
+  <div>
+    this is a huge test
+
+  </div>
+  <div>
+    this is a huge test
+
+  </div>
 </template>
 
 <script setup>
